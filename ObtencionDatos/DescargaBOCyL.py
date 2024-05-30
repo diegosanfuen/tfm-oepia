8cb78650ed7e450fe4a2ca2f8b1f2794befa26f1
Diego Sánchez <63495248+diegosanfuen@users.noreply.github.com>
2024-05-30 10:38:03 +0200

    ChatBot

--- ObtencionDatos/DescargaBOCyL.py
+++ ObtencionDatos/DescargaBOCyL.py
@@ -19,7 +19,7 @@ from sumy.utils import get_stop_words
 language = "spanish"
 
 # Introducir esta variable de entorno en el lanzador
-os.environ['PROJECT_ROOT'] = r'/content/content/tfm-oepia'
+# os.environ['PROJECT_ROOT'] = r'/content/tfm-oepia'
 
 # Abrir y leer el archivo YAML
 with open(Path(os.getenv('PROJECT_ROOT')) / 'config/config.yml', 'r') as file:
--- ObtencionDatos/__init__.py
+++ ObtencionDatos/__init__.py
@@ -1,7 +1,7 @@
 # Definimos una variable
 nombre_paquete = "ObtenerDatosCollab"
 import os
-os.environ['PROJECT_ROOT'] = r'/content/recuperacion_informacion_modelos_lenguaje/tfm'
+# os.environ['PROJECT_ROOT'] = r'/content/tfm-opeia'
 
 # Importamos los módulos que queremos que estén disponibles
 from ObtenerDatosCollab.DescargaBOE import DescargaBOE
