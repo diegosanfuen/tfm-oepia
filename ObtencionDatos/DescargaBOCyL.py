56adf842e3235c4661ef5810f3acc79537778d05
Diego Sánchez <63495248+diegosanfuen@users.noreply.github.com>
2024-05-31 12:29:50 +0200

    ChatBot

--- ObtencionDatos/DescargaBOCyL.py
+++ ObtencionDatos/DescargaBOCyL.py
@@ -176,6 +176,7 @@ class DescargaBOCyL:
         parsed_url = urlparse(url)
 
         dominio = parsed_url.netloc
+        dominio = dominio[:-1]
 
         response = requests.get(url)
         html_content = response.content
