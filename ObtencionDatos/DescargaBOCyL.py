a5d977565a563bbfd26d0b74c7d7648d5dfff7b4
Diego Sánchez <63495248+diegosanfuen@users.noreply.github.com>
2024-06-11 23:19:12 +0200

    ChatBot

--- ObtencionDatos/DescargaBOCyL.py
+++ ObtencionDatos/DescargaBOCyL.py
@@ -8,12 +8,11 @@ import re
 from pathlib import Path
 import logging, os, yaml, time
 
-# Continuar con el resto de tu código de sumy después de esta descarga
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
-from sumy.nlp.stemmers import Stemmer
-from sumy.utils import get_stop_words
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+from transformers import pipeline
+from transformers import AutoTokenizer
 
 # Idioma del texto
 language = "spanish"
